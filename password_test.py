93ded09f06f18146b8cf6a605d4bb96ae3887267
Berthedusabeyuzu <berthe1030@gmail.com>
2019-02-11 17:23:25 +0200

    test for new class

--- password_test.py
+++ password_test.py
@@ -33,8 +33,8 @@ class TestUser(unittest.TestCase):
         '''
         self.new_user.save_user() # saving the new user
         self.assertEqual(len(User.user_list),1)
-   
+    
 
 
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main() 

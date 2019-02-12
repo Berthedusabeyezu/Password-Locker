5676c041bc91393ded0a0d16df54d5d047a530ef
Berthedusabeyuzu <berthe1030@gmail.com>
2019-02-12 14:33:48 +0200

    creation read me

---
+++ ReadMe.md
@@ -0,0 +1,17 @@
+# project or program name
+Password Locker
+
+## author name
+Berthe DUSABEYEZU
+
+## description of project
+This project it deal with creating two classes and are authentication system that enables a user to log into their account and view their credentials. A user should be able to login to their account using the password that they created and NOT any password.
+
+## project setup instructions
+Must use chrome and edit textor
+
+## link to live site on GitHub Pages
+https://github.com/Berthedusabeyezu/Password-Locker
+
+## copyright and license information
+This project is under MIT License. Copyright (c) 2019 Berthe
--- run.py
+++ run.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3.6
 from credential import Credential
 
-    def create_credential(account_email,username,password):
-        '''
-        Function to create a new credential
-        '''
-        new_credential = Credential(account_email,username,password)
-        return new_credential
+def create_credential(account_email,username,password):
+    '''
+    Function to create a new credential
+    '''
+    new_credential = Credential(account_email,username,password)
+    return new_credential
     
     def save_credentials(credential): 
         ''' 
@@ -37,3 +37,75 @@ from credential import Credential
         Function that returns all the saved credentials
         '''
         return Credential.display_credentials()
+
+
+def main():
+    print("Hello Welcome to your credential list. What is your name?")
+    user_name = input()
+
+    print(f"Hello {user_name}. what would you like to do?")
+    print('\n')
+
+    while True:
+                print("Use these short codes : cc - create a new credential, dc - display credentials, fc -find a credential, ex -exit the credential list ")
+
+                short_code = input().lower()
+
+                if short_code == 'cc':
+                        print("New Credential")
+                        print("-"*10)
+
+                        print ("account_email ....")
+                        account_email = input()
+
+                        print("username ...")
+                        username = input()
+
+                        print("password ...")
+                        password= input()
+
+                        save_credential(create_credential(account_email,username,password)) # create and save new credential.
+                        print ('\n')
+                        print(f"New Credential {account_email,username,password}  created")
+                        print ('\n')
+
+                elif short_code == 'dc':
+
+                        if display_credentials():
+                                print("Here is a list of all your credentials")
+                                print('\n')
+
+                                for contact in display_credentials():
+                                    print(f"{credential.account_email} {contcredentialact.username} .....{credential.password}")
+
+                                print('\n')
+                        else:
+                                print('\n')
+                                print("You dont seem to have any credentials saved yet")
+                                print('\n')
+
+                elif short_code == 'fc':
+
+                            print("Enter the password you want to search for")
+
+                            search_password = input()
+                            if check_existing_credentials(search_password):
+                                    search_credential = find_credential(search_password)
+                                    print(f"{search_credential.account_email} {search_credential.username}")
+                                    print('-' * 20)
+
+                                    print(f"Password.......{search_credential.password}")
+                                    print(f"Account_email.......{search_credential.account_email}")
+                            else:
+                                    print("That credential does not exist")
+
+                elif short_code == "ex":
+                            print("Bye .......")
+                            break
+                else:
+                            print("I really didn't get that. Please use the short codes")
+
+
+if __name__ == '__main__':
+
+        main()
